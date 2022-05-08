6fbc375110768de089d56d81fb51e567bf1de3ca
yadullamalik <shaikyadullamalik123@gmail.com>
2022-05-08 16:01:33 +0530

    adding files
    adding files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Flights } from "./components/Flights/Flights";
+import { Product } from "./components/Product/Product";
+import { Stays } from "./components/Stays/Stays";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Stays /> */}
+      {/* <Flights /> */}
+      <Product />
+    </div>
+  );
+}
+
+export default App;
